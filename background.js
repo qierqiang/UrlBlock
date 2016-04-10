539d5e70732faec3a0cb83cc3a4b2c30e56a9a9a
qierqiang <erqiang.qi@gmail.com>
2016-04-10 16:53:44 +0800

    1

--- background.js
+++ background.js
@@ -1,20 +1,24 @@
 ﻿function getDomainFromUrl(url){
-	var host = "null";
-	if(typeof url == "undefined" || null == url)
-		url = window.location.href;
-	var regex = /.*\:\/\/([^\/]*).*/;
-	var match = url.match(regex);
-	if(typeof match != "undefined" && null != match)
-		host = match[1];
-	return host;
+//	var host = "null";
+//	if(typeof url == "undefined" || null == url)
+//		url = window.location.href;
+//	var regex = /.*\:\/\/([^\/]*).*/;
+//	var match = url.match(regex);
+//	if(typeof match != "undefined" && null != match)
+//		host = match[1];
+//	return host;
+
+    var a=document.createElement('a');
+    a.href=url;
+    return a.host; 
 }
 
 function checkForValidUrl(tabId, changeInfo, tab) {
     //if(tab.window.location.host.toLowerCase()==="")
 	
-	if(getDomainFromUrl(tab.url).toLowerCase()=="www.cnblogs.com"){
-		chrome.pageAction.show(tabId);
-        chrome.tabs.update(tabId.id,{url:"about:blank"});
+	if(getDomainFromUrl(tab.url).toLowerCase()=="123.hao245.com"){
+		//chrome.pageAction.show(tabId);
+        chrome.tabs.update(tabId.id,{url:"about:newtab"});
         //chrome.tab.url="about:blank";
 	}
 };
